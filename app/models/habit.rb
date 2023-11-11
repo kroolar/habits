0940cefd302fa64d66b459a0ca6c27dbc5ba7014
Artur Krol <kroolar@gmail.com>
2023-11-11 13:39:34 +0100

    Validate habit values

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,2 +1,7 @@
 class Habit < ApplicationRecord
+  alias_attribute :kind, :type
+
+  validates :name, :icon, :color, :kind, presence: true
+  validates :name, in: (3..)
+  validates :kind, inclusion: { in: %w[bad good] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_07_153856) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_11_123349) do
   create_table "habits", force: :cascade do |t|
     t.string "name"
     t.string "icon"
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_07_153856) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "kind"
   end
 
 end
